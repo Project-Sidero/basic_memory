2dbd938d3804434c264bc89e8419d26f9c0fe030
richard andrew cattermole <richard@cattermole.co.nz>
2022-06-14 09:23:42 +1200

    Introduce shared library workarounds

---
+++ source/sidero/base/moduleinfostubs.d
@@ -0,0 +1,56 @@
+module sidero.base.moduleinfostubs;
+
+static foreach (Stub; Stubs) {
+    mixin(() {
+        import std.format : format;
+
+        string ret = "export extern(C) void " ~ Stub.mangleName ~ "() { asm { naked; ";
+
+        void add(ubyte b) {
+            ret ~= "db 0x";
+
+            ubyte temp = b & 0xF;
+            if (temp > 9)
+                ret ~= 'A' + (temp - 10);
+            else
+                ret ~= '0' + temp;
+
+            temp = (b >> 4) & 0xF;
+            if (temp > 9)
+                ret ~= 'A' + (temp - 10);
+            else
+                ret ~= '0' + temp;
+
+            ret ~= ";";
+        }
+
+        add(MIname & 0xFF);
+        add((MIname >> 8) & 0xFF);
+        add(0);
+        add(0);
+
+        add(0);
+        add(0);
+        add(0);
+        add(0);
+
+        foreach (c; Stub.moduleName) {
+            add(c);
+        }
+        add(0);
+
+        return ret ~ "}\n}\n";
+    }());
+}
+
+private:
+struct ModuleInfoStub {
+    string mangleName;
+    string moduleName;
+}
+
+enum Stubs = [
+        ModuleInfoStub("_D6sidero4base10allocators2gc12__ModuleInfoZ", "sidero.base.allocators.gc"),
+        ModuleInfoStub("_D6sidero4base10allocators12__ModuleInfoZ", "sidero.base.allocators"),
+        ModuleInfoStub("_D6sidero4base10allocators3api12__ModuleInfoZ", "sidero.base.allocators.api")
+    ];
