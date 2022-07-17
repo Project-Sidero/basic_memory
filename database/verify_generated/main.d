043f41ef73c7bd2aafbc412fbbcda5e6b8823dc9
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2022-07-18 03:21:40 +1200

    Implement Unicode word break algorithm

--- database/verify_generated/main.d
+++ database/verify_generated/main.d
@@ -1,92 +1,14 @@
 ﻿module verify_generated.main;
-import verify_generated.testfile;
-import sidero.base.text.unicode.normalization;
-import sidero.base.text.unicode.database;
-import sidero.base.allocators.api;
-import sidero.base.allocators.predefined;
 import std.stdio;
 
-int main() {
-    try {
-        GeneralPurposeAllocator gpa;
-        RCAllocator allocator = RCAllocator.instanceOf(&gpa);
+void main() {
+    {
+        import verify_generated.normalizationtests;
+        import verify_generated.wordbreaktest;
 
-        writeln("============ NFD =============");
-
-        // NFD
-        foreach (testId, nte; normalizationTestEntries) {
-            writeln("Testing: ", testId, ": ", nte);
-            // source; NFC; NFD; NFKC; NFKD
-
-            // c3 ==  toNFD(c1) ==  toNFD(c2) ==  toNFD(c3)
-            assert(nte.NFD == toNFD(nte.source, allocator));
-            assert(nte.NFD == toNFD(nte.NFC, allocator));
-            assert(nte.NFD == toNFD(nte.NFD, allocator));
-
-            // c5 ==  toNFD(c4) ==  toNFD(c5)
-            assert(nte.NFKD == toNFD(nte.NFKC, allocator));
-            assert(nte.NFKD == toNFD(nte.NFKD, allocator));
-
-            allocator.deallocateAll;
-        }
-
-        writeln("============ NFKD =============");
-
-        // NFKD
-        foreach (testId, nte; normalizationTestEntries) {
-            writeln("Testing: ", testId, ": ", nte);
-            // source; NFC; NFD; NFKC; NFKD
-
-            // c5 == toNFKD(c1) == toNFKD(c2) == toNFKD(c3) == toNFKD(c4) == toNFKD(c5)
-            assert(nte.NFKD == toNFKD(nte.source, allocator));
-            assert(nte.NFKD == toNFKD(nte.NFC, allocator));
-            assert(nte.NFKD == toNFKD(nte.NFD, allocator));
-            assert(nte.NFKD == toNFKD(nte.NFKC, allocator));
-            assert(nte.NFKD == toNFKD(nte.NFKD, allocator));
-
-            allocator.deallocateAll;
-        }
-
-        writeln("============ NFC =============");
-
-        // NFC
-        foreach (testId, nte; normalizationTestEntries) {
-            writeln("Testing: ", testId, ": ", nte);
-            // source; NFC; NFD; NFKC; NFKD
-
-            // c2 ==  toNFC(c1) ==  toNFC(c2) ==  toNFC(c3)
-            assert(nte.NFC == toNFC(nte.source, allocator), "c2 == toNFC(c1)");
-            assert(nte.NFC == toNFC(nte.NFC, allocator), "c2 == toNFC(c2)");
-            assert(nte.NFC == toNFC(nte.NFD, allocator), "c2 == toNFC(c3)");
-
-            // c4 ==  toNFC(c4) ==  toNFC(c5)
-            assert(nte.NFKC == toNFC(nte.NFKC, allocator), "c4 == toNFC(c4)");
-            assert(nte.NFKC == toNFC(nte.NFKD, allocator), "c4 == toNFC(c5)");
-
-            allocator.deallocateAll;
-        }
-
-        writeln("============ NFKC =============");
-
-        // NFKC
-        foreach (testId, nte; normalizationTestEntries) {
-            writeln("Testing: ", testId, ": ", nte);
-            // source; NFC; NFD; NFKC; NFKD
-
-            // c4 == toNFKC(c1) == toNFKC(c2) == toNFKC(c3) == toNFKC(c4) == toNFKC(c5)
-            assert(nte.NFKC == toNFKC(nte.source, allocator));
-            assert(nte.NFKC == toNFKC(nte.NFC, allocator));
-            assert(nte.NFKC == toNFKC(nte.NFD, allocator));
-            assert(nte.NFKC == toNFKC(nte.NFKC, allocator));
-            assert(nte.NFKC == toNFKC(nte.NFKD, allocator));
-
-            allocator.deallocateAll;
-        }
-    } catch(Throwable e) {
-        writeln("Failure ", e.toString());
-        return -1;
+        normalizationTests;
+        wordBreakTests;
     }
 
     writeln("Success!");
-    return 0;
 }
