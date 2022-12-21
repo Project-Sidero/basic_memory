531a86fd646a9ce408ab419b8b03bd5a196225d8
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2022-12-22 06:19:11 +1300

    Format DllMain file differently

--- source/sidero/base/dllmain.d
+++ source/sidero/base/dllmain.d
@@ -1,14 +1,18 @@
 module sidero.base.dllmain;
 export:
 
-version (DynamicSideroBase)  : version (Windows)  : version (D_BetterC) {
-    import core.sys.windows.windef : HINSTANCE, BOOL, DWORD, LPVOID;
+version (DynamicSideroBase) {
+    version (Windows) {
+        version (D_BetterC) {
+            import core.sys.windows.windef : HINSTANCE, BOOL, DWORD, LPVOID;
 
-    extern (Windows) BOOL DllMain(HINSTANCE hInstance, DWORD ulReason, LPVOID reserved) {
-        return true;
+            extern (Windows) BOOL DllMain(HINSTANCE hInstance, DWORD ulReason, LPVOID reserved) {
+                return true;
+            }
+        } else {
+            import core.sys.windows.dll;
+
+            mixin SimpleDllMain;
+        }
     }
-} else {
-    import core.sys.windows.dll;
-
-    mixin SimpleDllMain;
 }
