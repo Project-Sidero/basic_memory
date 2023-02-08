767891357fd115fce7e451cd188c20cd21aaf4ed
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2023-02-09 12:19:15 +1300

    Some state filling out for logger

--- source/sidero/base/logger.d
+++ source/sidero/base/logger.d
@@ -3,8 +3,60 @@ import sidero.base.text;
 import sidero.base.attributes;
 import sidero.base.allocators;
 import sidero.base.errors;
+import sidero.base.console;
 
 export @safe nothrow @nogc:
+
+///
+enum LoggingTargets : uint {
+    /// Default
+    Console = 1,
+    ///
+    File = 1 << 1,
+    /// Posix, requires some sort of syslog
+    Syslog = 1 << 2,
+    /// Windows events
+    Windows = 1 << 3,
+    ///
+    Custom = 1 << 4,
+
+    ///
+    All = Console | File | Syslog | Windows | Custom,
+}
+
+///
+enum LogLevel {
+    /// Outputs everything
+    Trace,
+    /// Default, will only output info/warning/error/critical/fatal
+    Info,
+    /// Will only output warning/error/critical/fatal
+    Warning,
+    /// Will only output error/critical/fatal
+    Error,
+    /// Will only output critical/fatal
+    Critical,
+    /// Will only output fatal; this should (but won't) end the program
+    Fatal,
+}
+
+///
+enum LogRotateFrequency {
+    ///
+    None,
+    ///
+    OnStart,
+    ///
+    Hourly,
+    ///
+    Daily,
+}
+
+///
+alias CustomTargetMessage = void delegate(LogLevel level, String_UTF8 dateTime, String_UTF8[2] moduleLine,
+        String_UTF8 tags, String_UTF8 levelText, String_UTF8 composite) @safe nothrow @nogc;
+///
+alias CustomTargetOnRemove = void delegate() @safe nothrow @nogc;
 
 ///
 alias LoggerReference = ResultReference!Logger;
@@ -12,10 +64,23 @@ alias LoggerReference = ResultReference!Logger;
 ///
 struct Logger {
     private @PrettyPrintIgnore {
+        import sidero.base.containers.dynamicarray;
         import sidero.base.internal.meta;
 
         RCAllocator allocator;
         String_UTF8 name_;
+
+        ubyte[TestTestSetLockInline.sizeof] mutexStorage;
+        LogLevel logLevel;
+        uint targets;
+
+        ConsoleTarget consoleTarget;
+        FileTarget fileTarget;
+        DynamicArray!CustomTarget customTargets;
+
+        ref TestTestSetLockInline mutex() scope return nothrow @nogc {
+            return *cast(TestTestSetLockInline*)mutexStorage.ptr;
+        }
 
         static int opApplyImpl(Del)(scope Del del) @trusted {
             int result;
@@ -24,7 +89,7 @@ struct Logger {
                 return loggers;
             }
 
-            static if (__traits(compiles, {LoggerReference lr; del(lr);})) {
+            static if (__traits(compiles, { LoggerReference lr; del(lr); })) {
                 result = getLoggers.opApply(del);
             } else {
                 int handle()(ref ResultReference!String_UTF8 k, ref LoggerReference v) {
@@ -61,6 +126,11 @@ export:
     }
 
     ///
+    String_UTF8 toString() {
+        return this.name;
+    }
+
+    ///
     static LoggerReference forName(return scope String_UTF8 name, return scope RCAllocator allocator = RCAllocator.init) @trusted {
         if (name.length == 0)
             return typeof(return)(MalformedInputException("Name must not be empty"));
@@ -92,11 +162,89 @@ export:
     }
 }
 
+pragma(crt_destructor) extern (C) void deinitializeLogging() @trusted {
+    version (Posix) {
+        import core.sys.posix.syslog : closelog;
+
+        if (haveSysLog) {
+            closelog;
+            haveSysLog = false;
+        }
+    } else version (Windows) {
+        import core.sys.windows.windows : DeregisterEventSource;
+
+        if (windowsEventHandle !is null) {
+            DeregisterEventSource(windowsEventHandle);
+            windowsEventHandle = null;
+        }
+    }
+}
+
+unittest {
+    LoggerReference globalLogger = Logger.forName(String_UTF8("global"));
+    assert(globalLogger);
+    import sidero.base.console;
+
+    debugWriteln(globalLogger, globalLogger.name);
+}
+
 private:
 import sidero.base.containers.map.concurrenthashmap;
 import sidero.base.parallelism.mutualexclusion : TestTestSetLockInline;
+import sidero.base.internal.filesystem;
+import sidero.base.datetime;
 
 __gshared {
     TestTestSetLockInline mutexForCreation;
     ConcurrentHashMap!(String_UTF8, Logger) loggers;
+}
+
+struct ConsoleTarget {
+    ConsoleColor[2][6] consoleColors = [
+        [ConsoleColor.Yellow, ConsoleColor.Unknown], [ConsoleColor.Green, ConsoleColor.Unknown],
+        [ConsoleColor.Magenta, ConsoleColor.Unknown], [ConsoleColor.Red, ConsoleColor.Yellow],
+        [ConsoleColor.Red, ConsoleColor.Cyan], [ConsoleColor.Red, ConsoleColor.Blue],
+    ];
+    bool[6] useErrorStream = [false, false, false, true, true, true];
+}
+
+struct FileTarget {
+    String_UTF8 rootLogDirectory;
+
+    GDate logRotateLastDate;
+    LogRotateFrequency logRotateFrequency;
+
+    FileAppender logStream;
+
+@safe nothrow @nogc:
+
+    this(return scope ref FileTarget other) scope {
+    }
+}
+
+struct CustomTarget {
+    CustomTargetMessage messageDel;
+    CustomTargetOnRemove onRemoveDel;
+
+@safe nothrow @nogc:
+
+     ~this() scope {
+        if (onRemoveDel !is null)
+            onRemoveDel();
+    }
+}
+
+__gshared {
+    String_UTF8 processName;
+    bool haveSysLog;
+}
+
+version (Windows) {
+    import core.sys.windows.windows : HANDLE;
+
+    __gshared HANDLE windowsEventHandle;
+} else version (Posix) {
+    import core.sys.posix.syslog : LOG_DEBUG, LOG_INFO, LOG_NOTICE, LOG_WARNING, LOG_ERR;
+
+    static PrioritySyslogForLevels = [LOG_DEBUG, LOG_INFO, LOG_NOTICE, LOG_WARNING, LOG_ERR, LOG_ERR];
 }
