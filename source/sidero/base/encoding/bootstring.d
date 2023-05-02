d1a6c5b3669727a58951ed2c40cb3086e6125e6d
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2023-05-03 01:57:05 +1200

    Implement the BootString encode wrapper methods

--- source/sidero/base/encoding/bootstring.d
+++ source/sidero/base/encoding/bootstring.d
@@ -10,40 +10,133 @@ struct BootString(uint Base, uint Tmin, uint Tmax, uint Skew, uint Damp, uint In
     static assert(Base == 36, "BootString implementation only implemented for Punycode");
 export @safe nothrow @nogc static:
 
-    /*
-NOTE: encode must tell you if there was non-ASCII encoded
+    ///
+    Result!StringBuilder_ASCII encode(scope String_ASCII input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
 
-function encode:
-   let n = initial_n
-   let delta = 0
-   let bias = initial_bias
-   let h = b = the number of basic code points in the input
-   copy them to the output in order, followed by a delimiter if b > 0
-   {if the input contains a non-basic code point < n then fail}
-   while h < length(input) do begin
-     let m = the minimum {non-basic} code point >= n in the input
-     let delta = delta + (m - n) * (h + 1), fail on overflow
-     let n = m
-     for each code point c in the input (in order) do begin
-       if c < n {or c is basic} then increment delta, fail on overflow
-       if c == n then begin
-         let q = delta
-         for k = base to infinity in steps of base do begin
-           let t = tmin if k <= bias {+ tmin}, or
-                   tmax if k >= bias + tmax, or k - bias otherwise
-           if q < t then break
-           output the code point for digit t + ((q - t) mod (base - t))
-           let q = (q - t) div (base - t)
-         end
-         output the code point for digit q
-         let bias = adapt(delta, h + 1, test h equals b?)
-         let delta = 0
-         increment h
-       end
-     end
-     increment delta and n
-   end
- */
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope StringBuilder_ASCII input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope String_UTF8 input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope String_UTF16 input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope String_UTF32 input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope StringBuilder_UTF8 input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope StringBuilder_UTF16 input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    Result!StringBuilder_ASCII encode(scope StringBuilder_UTF32 input) {
+        StringBuilder_ASCII ret = StringBuilder_ASCII(globalAllocator());
+        auto result = encode(ret, input);
+
+        if (!result)
+            return typeof(return)(result.getError);
+        else
+            return typeof(return)(ret);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope String_ASCII input) {
+        return encode_(output, input);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope StringBuilder_ASCII input) {
+        return encode_(output, input);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope String_UTF8 input) {
+        return encode_(output, input.byUTF32);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope String_UTF16 input) {
+        return encode_(output, input.byUTF32);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope String_UTF32 input) {
+        return encode_(output, input);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope StringBuilder_UTF8 input) {
+        return encode_(output, input.byUTF32);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope StringBuilder_UTF16 input) {
+        return encode_(output, input.byUTF32);
+    }
+
+    ///
+    ErrorResult encode(scope StringBuilder_ASCII output, scope StringBuilder_UTF32 input) {
+        return encode_(output, input);
+    }
 
     ///
     Result!StringBuilder_UTF32 decode(scope String_ASCII input) {
@@ -98,6 +191,44 @@ function encode:
     }
 
 private:
+    ErrorResult encode_(Input)(scope StringBuilder_ASCII output, scope Input input) @trusted {
+        /*
+NOTE: encode must tell you if there was non-ASCII encoded
+
+function encode:
+   let n = initial_n
+   let delta = 0
+   let bias = initial_bias
+   let h = b = the number of basic code points in the input
+   copy them to the output in order, followed by a delimiter if b > 0
+   {if the input contains a non-basic code point < n then fail}
+   while h < length(input) do begin
+     let m = the minimum {non-basic} code point >= n in the input
+     let delta = delta + (m - n) * (h + 1), fail on overflow
+     let n = m
+     for each code point c in the input (in order) do begin
+       if c < n {or c is basic} then increment delta, fail on overflow
+       if c == n then begin
+         let q = delta
+         for k = base to infinity in steps of base do begin
+           let t = tmin if k <= bias {+ tmin}, or
+                   tmax if k >= bias + tmax, or k - bias otherwise
+           if q < t then break
+           output the code point for digit t + ((q - t) mod (base - t))
+           let q = (q - t) div (base - t)
+         end
+         output the code point for digit q
+         let bias = adapt(delta, h + 1, test h equals b?)
+         let delta = 0
+         increment h
+       end
+     end
+     increment delta and n
+   end
+ */
+        assert(0);
+    }
+
     ErrorResult decode_(Input)(scope StringBuilder_UTF32 output, scope Input input) @trusted {
         dchar toInsert = InitialN;
         const startingOutputLength = output.length;
@@ -215,14 +346,18 @@ alias Punycode = BootString!(36, 1, 26, 38, 700, 72, 128, '-');
 unittest {
     import sidero.base.text.unicode.comparison;
     import sidero.base.console;
+
     assert(Punycode.decode(String_ASCII("Hello World!-wi44b")).assumeOkay == "Hello \u9EDEWorld!");
-    assert(Punycode.decode(String_ASCII("-egbpdaj6bu4bxfgehfvwxn")).assumeOkay == "\u0644\u064A\u0647\u0645\u0627\u0628\u062A\u0643\u0644\u0645\u0648\u0634\u0639\u0631\u0628\u064A\u061F");
+    assert(Punycode.decode(String_ASCII("-egbpdaj6bu4bxfgehfvwxn"))
+            .assumeOkay == "\u0644\u064A\u0647\u0645\u0627\u0628\u062A\u0643\u0644\u0645\u0648\u0634\u0639\u0631\u0628\u064A\u061F");
     assert(Punycode.decode(String_ASCII("-ihqwcrb4cv8a8dqg056pqjye")).assumeOkay == "\u4ED6\u4EEC\u4E3A\u4EC0\u4E48\u4E0D\u8BF4\u4E2D\u6587");
-    assert(Punycode.decode(String_ASCII("-ihqwctvzc91f659drss3x8bo0yb")).assumeOkay == "\u4ED6\u5011\u7232\u4EC0\u9EBD\u4E0D\u8AAA\u4E2D\u6587");
-    assert(Punycode.decode(String_ASCII("Proprostnemluvesky-uyb24dma41a")).assumeOkay== "\u0050\u0072\u006F\u010D\u0070\u0072\u006F\u0073\u0074\u011B\u006E\u0065\u006D\u006C\u0075\u0076\u00ED\u010D\u0065\u0073\u006B\u0079");
+    assert(Punycode.decode(String_ASCII("-ihqwctvzc91f659drss3x8bo0yb"))
+            .assumeOkay == "\u4ED6\u5011\u7232\u4EC0\u9EBD\u4E0D\u8AAA\u4E2D\u6587");
+    assert(Punycode.decode(String_ASCII("Proprostnemluvesky-uyb24dma41a")).assumeOkay == "\u0050\u0072\u006F\u010D\u0070\u0072\u006F\u0073\u0074\u011B\u006E\u0065\u006D\u006C\u0075\u0076\u00ED\u010D\u0065\u0073\u006B\u0079");
     assert(Punycode.decode(String_ASCII("-4dbcagdahymbxekheh6e0a7fei0b")).assumeOkay == "\u05DC\u05DE\u05D4\u05D4\u05DD\u05E4\u05E9\u05D5\u05D8\u05DC\u05D0\u05DE\u05D3\u05D1\u05E8\u05D9\u05DD\u05E2\u05D1\u05E8\u05D9\u05EA");
     assert(Punycode.decode(String_ASCII("-i1baa7eci9glrd9b2ae1bj0hfcgg6iyaf8o0a1dig0cd")).assumeOkay == "\u092F\u0939\u0932\u094B\u0917\u0939\u093F\u0928\u094D\u0926\u0940\u0915\u094D\u092F\u094B\u0902\u0928\u0939\u0940\u0902\u092C\u094B\u0932\u0938\u0915\u0924\u0947\u0939\u0948\u0902");
-    assert(Punycode.decode(String_ASCII("-n8jok5ay5dzabd5bym9f0cm5685rrjetr6pdxa")).assumeOkay == "\u306A\u305C\u307F\u3093\u306A\u65E5\u672C\u8A9E\u3092\u8A71\u3057\u3066\u304F\u308C\u306A\u3044\u306E\u304B");
+    assert(Punycode.decode(String_ASCII("-n8jok5ay5dzabd5bym9f0cm5685rrjetr6pdxa"))
+            .assumeOkay == "\u306A\u305C\u307F\u3093\u306A\u65E5\u672C\u8A9E\u3092\u8A71\u3057\u3066\u304F\u308C\u306A\u3044\u306E\u304B");
     assert(Punycode.decode(String_ASCII("-989aomsvi5e83db1d2a355cv1e0vak1dwrv93d5xbh15a0dt30a5jpsd879ccm6fea98c")).assumeOkay == "\uC138\uACC4\uC758\uBAA8\uB4E0\uC0AC\uB78C\uB4E4\uC774\uD55C\uAD6D\uC5B4\uB97C\uC774\uD574\uD55C\uB2E4\uBA74\uC5BC\uB9C8\uB098\uC88B\uC744\uAE4C");
     assert(Punycode.decode(String_ASCII("-b1abfaaepdrnnbgefbaDotcwatmq2g4l")).assumeOkay == "\u043F\u043E\u0447\u0435\u043C\u0443\u0436\u0435\u043E\u043D\u0438\u043D\u0435\u0433\u043E\u0432\u043E\u0440\u044F\u0442\u043F\u043E\u0440\u0443\u0441\u0441\u043A\u0438");
     assert(Punycode.decode(String_ASCII("PorqunopuedensimplementehablarenEspaol-fmd56a")).assumeOkay == "\u0050\u006F\u0072\u0071\u0075\u00E9\u006E\u006F\u0070\u0075\u0065\u0064\u0065\u006E\u0073\u0069\u006D\u0070\u006C\u0065\u006D\u0065\u006E\u0074\u0065\u0068\u0061\u0062\u006C\u0061\u0072\u0065\u006E\u0045\u0073\u0070\u0061\u00F1\u006F\u006C");
@@ -231,7 +366,8 @@ unittest {
     assert(Punycode.decode(String_ASCII("-with-SUPER-MONKEYS-pc58ag80a8qai00g7n9n")).assumeOkay == "\u5B89\u5BA4\u5948\u7F8E\u6075\u002D\u0077\u0069\u0074\u0068\u002D\u0053\u0055\u0050\u0045\u0052\u002D\u004D\u004F\u004E\u004B\u0045\u0059\u0053");
     assert(Punycode.decode(String_ASCII("Hello-Another-Way--fc4qua05auwb3674vfr0b")).assumeOkay == "\u0048\u0065\u006C\u006C\u006F\u002D\u0041\u006E\u006F\u0074\u0068\u0065\u0072\u002D\u0057\u0061\u0079\u002D\u305D\u308C\u305E\u308C\u306E\u5834\u6240");
     assert(Punycode.decode(String_ASCII("2-u9tlzr9756bt3uc0v")).assumeOkay == "\u3072\u3068\u3064\u5C4B\u6839\u306E\u4E0B\u0032");
-    assert(Punycode.decode(String_ASCII("MajiKoi5-783gue6qz075azm5e")).assumeOkay == "\u004D\u0061\u006A\u0069\u3067\u004B\u006F\u0069\u3059\u308B\u0035\u79D2\u524D");
+    assert(Punycode.decode(String_ASCII("MajiKoi5-783gue6qz075azm5e"))
+            .assumeOkay == "\u004D\u0061\u006A\u0069\u3067\u004B\u006F\u0069\u3059\u308B\u0035\u79D2\u524D");
     assert(Punycode.decode(String_ASCII("de-jg4avhby1noc0d")).assumeOkay == "\u30D1\u30D5\u30A3\u30FC\u0064\u0065\u30EB\u30F3\u30D0");
     assert(Punycode.decode(String_ASCII("-d9juau41awczczp")).assumeOkay == "\u305D\u306E\u30B9\u30D4\u30FC\u30C9\u3067");
     assert(Punycode.decode(String_ASCII("-> $1.00 <--")).assumeOkay == "\u002D\u003E\u0020\u0024\u0031\u002E\u0030\u0030\u0020\u003C\u002D");
