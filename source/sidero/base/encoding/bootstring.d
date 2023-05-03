9495687e404e55d593c84d807db6bd6d3465d5f3
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2023-05-03 23:43:56 +1200

    Implement BootString encoding

--- source/sidero/base/encoding/bootstring.d
+++ source/sidero/base/encoding/bootstring.d
@@ -192,41 +192,71 @@ export @safe nothrow @nogc static:
 
 private:
     ErrorResult encode_(Input)(scope StringBuilder_ASCII output, scope Input input) @trusted {
-        /*
-NOTE: encode must tell you if there was non-ASCII encoded
+        size_t n = InitialN, delta, bias = InitialBias, numberOfBasicCharacters;
 
-function encode:
-   let n = initial_n
-   let delta = 0
-   let bias = initial_bias
-   let h = b = the number of basic code points in the input
-   copy them to the output in order, followed by a delimiter if b > 0
-   {if the input contains a non-basic code point < n then fail}
-   while h < length(input) do begin
-     let m = the minimum {non-basic} code point >= n in the input
-     let delta = delta + (m - n) * (h + 1), fail on overflow
-     let n = m
-     for each code point c in the input (in order) do begin
-       if c < n {or c is basic} then increment delta, fail on overflow
-       if c == n then begin
-         let q = delta
-         for k = base to infinity in steps of base do begin
-           let t = tmin if k <= bias {+ tmin}, or
-                   tmax if k >= bias + tmax, or k - bias otherwise
-           if q < t then break
-           output the code point for digit t + ((q - t) mod (base - t))
-           let q = (q - t) div (base - t)
-         end
-         output the code point for digit q
-         let bias = adapt(delta, h + 1, test h equals b?)
-         let delta = 0
-         increment h
-       end
-     end
-     increment delta and n
-   end
- */
-        assert(0);
+        if (input.length == 0)
+            return ErrorResult.init;
+
+        ubyte encodeDigit(uint c, int flag) {
+            return cast(ubyte)(c + 22 + 75 * (c < 26) - ((flag != 0) << 5));
+        }
+
+        foreach (c; input) {
+            if (c < 0x80) {
+                // basic character
+                output ~= [cast(ubyte)c];
+                numberOfBasicCharacters++;
+            }
+        }
+
+        output ~= [Deliminator];
+        size_t amountConsumed = numberOfBasicCharacters;
+
+        while (amountConsumed < input.encodingLength) {
+            uint m = uint.max;
+
+            foreach(c; input) {
+                if (c >= n && c < m)
+                    m = c;
+            }
+
+            if (m - n > (uint.max - delta) / (amountConsumed + 1))
+                return ErrorResult(MalformedInputException("Error delta location calculation would overflow"));
+            delta += (m - n) * (amountConsumed + 1);
+            n = m;
+
+            foreach (c; input) {
+                if (c < n || c < 0x80) {
+                    delta++;
+                    if (delta == 0)
+                        return ErrorResult(MalformedInputException("Error delta location calculation has overflown"));
+                }
+
+                if (c == n) {
+                    uint q = cast(uint)delta;
+
+                    for (size_t k = Base;; k += Base) {
+                        const t = k <= bias ? Tmin : (k >= bias + Tmax) ? Tmax : (k - bias);
+
+                        if (q < t)
+                            break;
+
+                        output ~= [encodeDigit(cast(uint)(t + ((q - t) % (Base - t))), 0)];
+                        q = cast(uint)((q - t) / (Base - t));
+                    }
+
+                    output ~= [encodeDigit(q, 0)];
+                    bias = adapt(delta, amountConsumed + 1, amountConsumed == numberOfBasicCharacters);
+                    delta = 0;
+                    amountConsumed++;
+                }
+            }
+
+            delta++;
+            n++;
+        }
+
+        return ErrorResult.init;
     }
 
     ErrorResult decode_(Input)(scope StringBuilder_UTF32 output, scope Input input) @trusted {
@@ -343,10 +373,8 @@ function encode:
 ///
 alias Punycode = BootString!(36, 1, 26, 38, 700, 72, 128, '-');
 
+///
 unittest {
-    import sidero.base.text.unicode.comparison;
-    import sidero.base.console;
-
     assert(Punycode.decode(String_ASCII("Hello World!-wi44b")).assumeOkay == "Hello \u9EDEWorld!");
     assert(Punycode.decode(String_ASCII("-egbpdaj6bu4bxfgehfvwxn"))
             .assumeOkay == "\u0644\u064A\u0647\u0645\u0627\u0628\u062A\u0643\u0644\u0645\u0648\u0634\u0639\u0631\u0628\u064A\u061F");
@@ -372,4 +400,30 @@ unittest {
     assert(Punycode.decode(String_ASCII("-d9juau41awczczp")).assumeOkay == "\u305D\u306E\u30B9\u30D4\u30FC\u30C9\u3067");
     assert(Punycode.decode(String_ASCII("-> $1.00 <--")).assumeOkay == "\u002D\u003E\u0020\u0024\u0031\u002E\u0030\u0030\u0020\u003C\u002D");
     assert(Punycode.decode(String_ASCII("-")).assumeOkay == "");
+}
+
+///
+unittest {
+    import sidero.base.console;
+    assert(Punycode.encode(String_UTF8("Hello \u9EDEWorld!")).assumeOkay == "Hello World!-wi44b");
+    assert(Punycode.encode(String_UTF8("\u0644\u064A\u0647\u0645\u0627\u0628\u062A\u0643\u0644\u0645\u0648\u0634\u0639\u0631\u0628\u064A\u061F")).assumeOkay == "-egbpdaj6bu4bxfgehfvwxn");
+    assert(Punycode.encode(String_UTF8("\u4ED6\u4EEC\u4E3A\u4EC0\u4E48\u4E0D\u8BF4\u4E2D\u6587")).assumeOkay == "-ihqwcrb4cv8a8dqg056pqjye");
+    assert(Punycode.encode(String_UTF8("\u4ED6\u5011\u7232\u4EC0\u9EBD\u4E0D\u8AAA\u4E2D\u6587")).assumeOkay == "-ihqwctvzc91f659drss3x8bo0yb");
+    assert(Punycode.encode(String_UTF8("\u0050\u0072\u006F\u010D\u0070\u0072\u006F\u0073\u0074\u011B\u006E\u0065\u006D\u006C\u0075\u0076\u00ED\u010D\u0065\u0073\u006B\u0079")).assumeOkay == "Proprostnemluvesky-uyb24dma41a");
+    assert(Punycode.encode(String_UTF8("\u05DC\u05DE\u05D4\u05D4\u05DD\u05E4\u05E9\u05D5\u05D8\u05DC\u05D0\u05DE\u05D3\u05D1\u05E8\u05D9\u05DD\u05E2\u05D1\u05E8\u05D9\u05EA")).assumeOkay == "-4dbcagdahymbxekheh6e0a7fei0b");
+    assert(Punycode.encode(String_UTF8("\u092F\u0939\u0932\u094B\u0917\u0939\u093F\u0928\u094D\u0926\u0940\u0915\u094D\u092F\u094B\u0902\u0928\u0939\u0940\u0902\u092C\u094B\u0932\u0938\u0915\u0924\u0947\u0939\u0948\u0902")).assumeOkay == "-i1baa7eci9glrd9b2ae1bj0hfcgg6iyaf8o0a1dig0cd");
+    assert(Punycode.encode(String_UTF8("\u306A\u305C\u307F\u3093\u306A\u65E5\u672C\u8A9E\u3092\u8A71\u3057\u3066\u304F\u308C\u306A\u3044\u306E\u304B")).assumeOkay == "-n8jok5ay5dzabd5bym9f0cm5685rrjetr6pdxa");
+    assert(Punycode.encode(String_UTF8("\uC138\uACC4\uC758\uBAA8\uB4E0\uC0AC\uB78C\uB4E4\uC774\uD55C\uAD6D\uC5B4\uB97C\uC774\uD574\uD55C\uB2E4\uBA74\uC5BC\uB9C8\uB098\uC88B\uC744\uAE4C")).assumeOkay == "-989aomsvi5e83db1d2a355cv1e0vak1dwrv93d5xbh15a0dt30a5jpsd879ccm6fea98c");
+    assert(Punycode.encode(String_UTF8("\u043F\u043E\u0447\u0435\u043C\u0443\u0436\u0435\u043E\u043D\u0438\u043D\u0435\u0433\u043E\u0432\u043E\u0440\u044F\u0442\u043F\u043E\u0440\u0443\u0441\u0441\u043A\u0438")).assumeOkay == "-b1abfaaepdrnnbgefbadotcwatmq2g4l");
+    assert(Punycode.encode(String_UTF8("\u0050\u006F\u0072\u0071\u0075\u00E9\u006E\u006F\u0070\u0075\u0065\u0064\u0065\u006E\u0073\u0069\u006D\u0070\u006C\u0065\u006D\u0065\u006E\u0074\u0065\u0068\u0061\u0062\u006C\u0061\u0072\u0065\u006E\u0045\u0073\u0070\u0061\u00F1\u006F\u006C")).assumeOkay == "PorqunopuedensimplementehablarenEspaol-fmd56a");
+    assert(Punycode.encode(String_UTF8("\u0054\u1EA1\u0069\u0073\u0061\u006F\u0068\u1ECD\u006B\u0068\u00F4\u006E\u0067\u0074\u0068\u1EC3\u0063\u0068\u1EC9\u006E\u00F3\u0069\u0074\u0069\u1EBF\u006E\u0067\u0056\u0069\u1EC7\u0074")).assumeOkay == "TisaohkhngthchnitingVit-kjcr8268qyxafd2f1b9g");
+    assert(Punycode.encode(String_UTF8("\u0033\u5E74\u0042\u7D44\u91D1\u516B\u5148\u751F")).assumeOkay == "3B-ww4c5e180e575a65lsy2b");
+    assert(Punycode.encode(String_UTF8("\u5B89\u5BA4\u5948\u7F8E\u6075\u002D\u0077\u0069\u0074\u0068\u002D\u0053\u0055\u0050\u0045\u0052\u002D\u004D\u004F\u004E\u004B\u0045\u0059\u0053")).assumeOkay == "-with-SUPER-MONKEYS-pc58ag80a8qai00g7n9n");
+    assert(Punycode.encode(String_UTF8("\u0048\u0065\u006C\u006C\u006F\u002D\u0041\u006E\u006F\u0074\u0068\u0065\u0072\u002D\u0057\u0061\u0079\u002D\u305D\u308C\u305E\u308C\u306E\u5834\u6240")).assumeOkay == "Hello-Another-Way--fc4qua05auwb3674vfr0b");
+    assert(Punycode.encode(String_UTF8("\u3072\u3068\u3064\u5C4B\u6839\u306E\u4E0B\u0032")).assumeOkay == "2-u9tlzr9756bt3uc0v");
+    assert(Punycode.encode(String_UTF8("\u004D\u0061\u006A\u0069\u3067\u004B\u006F\u0069\u3059\u308B\u0035\u79D2\u524D")).assumeOkay == "MajiKoi5-783gue6qz075azm5e");
+    assert(Punycode.encode(String_UTF8("\u30D1\u30D5\u30A3\u30FC\u0064\u0065\u30EB\u30F3\u30D0")).assumeOkay == "de-jg4avhby1noc0d");
+    assert(Punycode.encode(String_UTF8("\u305D\u306E\u30B9\u30D4\u30FC\u30C9\u3067")).assumeOkay == "-d9juau41awczczp");
+    assert(Punycode.encode(String_UTF8("\u002D\u003E\u0020\u0024\u0031\u002E\u0030\u0030\u0020\u003C\u002D")).assumeOkay == "-> $1.00 <--");
+    assert(Punycode.encode(String_UTF8("")).assumeOkay == "");
 }
